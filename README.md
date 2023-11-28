ee2188ce118ced3ef46749c2f265cdf3226f32dc
王傳馨 <flora@wangchuanxindeMacBook-Air.local>
2023-11-28 12:55:43 +0800

    index work

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-# 上傳資料
-curl -X POST "localhost:9200/law_test_data/_bulk" -H 'Content-Type: application/json' --data-binary @law_test.json
 # run docker
 docker-compose up --build
+# 上傳資料
+curl -X POST "localhost:9200/law_test_data/_bulk" -H 'Content-Type: application/json' --data-binary @law_test.json
+# 搜尋
+curl -X GET "localhost:9200/law_test_data/_search" -H 'Content-Type: application/json' -d '{
+  "query": {
+    "match": {
+      "content": "繼承人"          
+    }
+  }
+}'
--- web/app/app.py
+++ web/app/app.py
@@ -7,20 +7,26 @@ app = Flask(__name__)
 ELASTICSEARCH_URL = "http://localhost:9200"
 INDEX_NAME = "test_data"
 
+print("hello")
 
 @app.route('/')
 def index():
+    print("hello4")
     return render_template('index.html')
 
 
 @app.route('/search', methods=['POST'])
 def search():
+    print("hello 2")
     search_term = request.form['search_term']
     results = perform_elasticsearch_search(search_term)
+    print(results)
+    return "hello3"
     return render_template('index.html', results=results, search_term=search_term)
 
 
 def perform_elasticsearch_search(search_term):
+    print("search_term")
     search_url = f"{ELASTICSEARCH_URL}/{INDEX_NAME}/_search"
     query = {
         "query": {
@@ -31,9 +37,10 @@ def perform_elasticsearch_search(search_term):
     }
     headers = {'Content-Type': 'application/json'}
     response = requests.get(search_url, headers=headers, data=json.dumps(query))
+    return {"key":"value"}
     results = response.json().get('hits', {}).get('hits', [])
     return results
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',port=5000,debug=True)
